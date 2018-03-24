990bda5668a20923348e4f1e5d9d7aaf31805dbe
Ryan Sidebottom <rmsidebottom0@gmail.com>
2018-03-24 19:02:29 -0400

    Added more functionality. Debugging prompting user for data.

--- PhilipsHue/api.py
+++ PhilipsHue/api.py
@@ -1,64 +1,121 @@
+""" Philips Hue Bridge program to access the API remotely and manage it through
+    a more user friendly interface.
+
+    Author: Ryan Sidebottom
+    Date: 3/24/17
+"""
+
 import sys, json
 
-menu = {}
+# menu = {}
+body = {}
 # format(', '.join(list(menu.keys())))
 
+"""
+    At the last level in a dictionary, only values can be changed.
+    Get data to be changed (in json format), get request type, or do nothing
+"""
+def lastlLevel(dct):
+    print("Options: get, put, post, exit")
+    act = input("Enter choice: ")
+
+    while act.lower() != 'exit':
+        if act.lower() == 'get':
+            req = ''
+            return req
+        elif act.lower() == 'put':
+            # ask user for data to put
+            while True:
+                print("Enter data in json format to be changed.")
+                print("Values that can be changed are: \
+                    {}".format(', '.join(list(dct))))
+                data = input("Enter data: ")
+                if type(data) == type(dct):
+                    body = data
+                    return ''
+        elif act.lower() == 'post':
+            req = ''
+            break
+        # they enter something that isn't valid
+        else:
+            print("Options: get, put, post, exit")
+            act = input("Enter choice: ")
+    return ''
+
+""" Function takes a list of options, displays them, accepts input and
+    checks the input to ensure it is valid.
+    Sets the global variable body to the body of the message if there is a body.
+    Returns the path of the object.
+    Returns '' when exit pressed.
+"""
+def runList(keyDict):
+    choice=""
+    req=""
+    opts=['get', 'put', 'post', 'deeper', 'exit']
+    subDict = False
+    for val in keyDict.values():
+        # print("{0}:::{1}".format(type(val), val))
+        if type(val) == type({"":""}):
+            subDict = True
+
+    if not subDict:
+        # call function to get values or set get request, can't traverse any
+        # lower
+        req = lastlLevel(keyDict)
+        return req
+
+    while choice.lower() != 'exit':
+        # prompt user to pick one of the keys at the nth level
+        print("Choose one of the following: {}".format(', '.join(list(keyDict.keys()))))
+        choice = input("Enter choice:")
+        if choice.lower() in [x.lower() for x in list(keyDict.keys())]:
+            #prompt user for actions based on choice
+            print("What do you want to do with {0}: {1}".format(choice,
+            ', '.join(opts)))
+            act = input("Enter choice: ")
+
+            # loop through options, prompt user to finish the options
+            # options refer to all of the keys at the current level
+            # one request to the API can support changing multiple values
+            while act.lower() != 'exit':
+                if act.lower() == 'get':
+                    req = choice
+                    return req
+                elif act.lower() == 'put':
+                    # ask user for data to put
+                    # TODO
+                    break
+                elif act.lower() == 'post':
+                    req = ''
+                    return req
+                elif act.lower() == 'deeper':
+                    # go one level deeper
+                    req = choice +"/"+ runList(keyDict[choice])
+                    return req
+                else:
+                    print("Please choose an option {1}".format(choice, \
+                    ', '.join(opts)))
+                    act = input("Enter choice: ")
+                # prompt the user for any other values here
+                print("What do you want to do with {0}: {1}".format(choice,
+                ', '.join(opts)))
+                act = input("Enter choice: ")
+
+    return req;
+
 """ This runs the code
-    ip is the IP/hostname of the server
+    ip: the IP/hostname of the server
+    user: username required for access to the API
 """
 def main(ip, user):
     with open("/Users/daboss/Documents/Git/SmartHome/PhilipsHue/options.json",
         encoding='utf-8-sig') as json_file:
         text = json_file.read()
         menu = json.loads(text)
-    lvl1=list(menu.keys())
-    print(lvl1)
-    choice = ""
-    while choice.lower() != 'exit':
-        print("Enter exit to quit.")
-        # read std input for light, groups, config, schedules, scenes, rules, sensors
-        ### MAKE A FUCNTION TO DO THIS, SUPPLY IT LIST TO PRINT OUT AND CHECK
-        ### IF RESPONSE IS IN LIST.
-        while True:
-            print("Choose one of the following: {}".format(', '.join(lvl1)))
-            choice = input("Enter choice:")
-            if choice.lower() in lvl1:
-                return;
-        lvl2 = options(choice)
+    print(type(menu))
+    req = "/api/"+user+"/"+runList(menu)
 
-
-""" This function will print the options available to the user for their
-    current screen.
-    Returns path to requested item as a string """
-def options(keyword):
-    # /api/key/lvl1/lvl2/etc.
-    # Light: list, pick number, state:on, state:bri, name, getStats
-    # getStats: return state:on, state:bri, state:alert, type, name, modelid,
-    # uniqueid
-    if keyword.lower() == "light":
-        path = "/lights"
-
-    # Groups: list, pick room, roomStats
-    # roomStats: list name, lights, type, class
-
-    # config: info, time, version, whitelist
-    # info: name, bridgeid, mac, dhcp, ipaddress, netmask, gateway, proxy
-    # time: UTC, localtime, timezone
-    # version: modelid, datastoreversion, swversion, apiversion
-    # whitelist: id:name, id:last use date
-
-    # schedules: list
-
-    # scenes: list, id:info
-    # list: id:name for each scene
-    # id:info: name, lights, owner, locked, lastupdated
-
-    # rules: list
-
-    # sensors: list, #:state, #:config, info
-    # info: name, type, modelid, manufacturername, swversion
-
-    # resourcelinks: list
+    print(req)
 
 if __name__ == "__main__":
     # if len(sys.argv) < 3:
